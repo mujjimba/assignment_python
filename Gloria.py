e36f8b7877c8f85f75d14d9339f347170e5a62bb
mujjimba <143530026+mujjimba@users.noreply.github.com>
2023-09-11 06:41:16 -0700

    Add files via upload

---
+++ Gloria.py
@@ -0,0 +1,51 @@
+#even numbers
+milosh = (2, 4,6)
+sum_of_milosh = (2+4+6)
+print(sum_of_milosh)
+print("sum of even numbers less than 8" +str(sum_of_milosh))
+      
+      #random numbers from keyboard
+milosh = (6,5,7,8,9)
+sum_of_milosh =(6+5+7+8+9)
+      #print(sum_milosh)
+Average =(sum(milosh)/5)
+print(Average)
+print("|Average" +str(Average))
+
+#Operations
+#questions a
+-5 + 8 *6
+margie = -5
+debbie  = 8
+folly = 6
+kenn = (debbie*folly)
+print(margie+kenn)
+
+        #question b
+(55+9) %9
+pelly =55
+ketra =9
+gigi =(pelly +ketra)
+print(gigi%ketra)
+
+#question c
+20 + -3*5/8
+fassy =20
+agie = -3
+eddie = 5
+jera = 8
+mark =(agie*eddie)
+jack =(mark/jera)
+
+#question d
+5 + 15/3*2-8%3
+kal = 5
+mumiyo = 15
+fic =3
+etu =2
+lala =8
+ffi =3
+jky =(fic*etu)
+kagi = (mumiyo/jky)
+apo = (kagi-ffi)
+print(kal + apo)                                                                
